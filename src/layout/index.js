d62f4245060a3dca628c47a241d9dd936d098bb7
asso1985 <assadi.omar.1985@gmail.com>
2020-04-27 09:22:23 +0200

    Added media queries

--- src/layout/index.js
+++ src/layout/index.js
@@ -15,17 +15,31 @@ const Section = styled.section`
   flex-grow: 1;
   margin: 0 auto;
   max-width: 935px;
-  padding-top: 35px;
   display: flex;
+  flex-direction: column;
+  padding-top: 20px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 35px;
+    flex-direction: row;
+  }
 `;
 
 const PostArea = styled.div`
   float: left;
   margin-right: 28px;
-  max-width: 614px;
   width: 100%;
+
+  @media screen and (min-width: 768px) {
+    max-width: 614px;
+  }
 `;
 
-const SideArea = styled.div``;
+const SideArea = styled.div`
+  padding: 0 16px;
+  @media screen and (min-width: 768px) {
+    padding: 0;
+  }
+`;
 
 export { AppContainer, Section, Main, PostArea, SideArea };
