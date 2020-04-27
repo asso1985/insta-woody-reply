18274fc20239700f2cf45eed84b89ce3d6409417
asso1985 <assadi.omar.1985@gmail.com>
2020-04-27 06:50:11 +0200

    Added content loader

---
+++ src/components/Avatar/AvatarLoader.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const SIZE_MAP = {
+  thin: "20px",
+  small: "32px",
+  standard: "50px",
+};
+
+const ItemPicLoader = ({ size }) => {
+  return (
+    <div
+      height="100%"
+      width="100%"
+      style={{ borderRadius: "100%", overflow: "hidden", display: "flex" }}
+    >
+      <ContentLoader
+        speed={2}
+        backgroundColor="#f2f2f2"
+        foregroundColor="#ddd"
+        height={SIZE_MAP[size]}
+        width={SIZE_MAP[size]}
+        viewBox={`0 0 ${SIZE_MAP[size]} ${SIZE_MAP[size]}`}
+      >
+        <rect x="0" y="0" height={SIZE_MAP[size]} width={SIZE_MAP[size]}></rect>
+      </ContentLoader>
+    </div>
+  );
+};
+
+export default ItemPicLoader;
